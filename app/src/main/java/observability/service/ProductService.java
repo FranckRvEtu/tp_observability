e0c30d071bb687b30dabb6a76c5fe4812a7225f3
Franck Reveille <franck.reveille@etu.umontpellier.fr>
2025-12-07 18:08:22 +0100

    Reversed Spoon integration : need to be automated

--- app/src/main/java/observability/service/ProductService.java
+++ app/src/main/java/observability/service/ProductService.java
@@ -16,47 +16,42 @@ import org.slf4j.LoggerFactory;
 public class ProductService {
 
     private final ProductRepository productRepository;
-
     @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     public List<Product> findAll() {
-        LoggerFactory.getLogger("profile.logger").info("{\"timestamp\": \"" + java.time.Instant.now() + "\", \"user\": \"" +UserContext.getCurrentUser() + "\", \"action\": \"READ\", \"method\": \"findAll\"}");;
         return productRepository.findAll();
     }
 
     public Product findById(String id) {
-        LoggerFactory.getLogger("profile.logger").info("{\"timestamp\": \"" + java.time.Instant.now() + "\", \"user\": \"" +UserContext.getCurrentUser() + "\", \"action\": \"READ\", \"method\": \"findById\"}");;
-        return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Produit non trouvé avec l'id : " + id));
+        return productRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException("Produit non trouvé avec l'id : "+id)
+        );
     }
 
     public Product insert(Product product) {
-        LoggerFactory.getLogger("profile.logger").info("{\"timestamp\": \"" + java.time.Instant.now() + "\", \"user\": \"" +UserContext.getCurrentUser() + "\", \"action\": \"WRITE\", \"method\": \"insert\"}");;
         return productRepository.insert(product);
     }
 
     public void delete(String id) {
-        LoggerFactory.getLogger("profile.logger").info("{\"timestamp\": \"" + java.time.Instant.now() + "\", \"user\": \"" +UserContext.getCurrentUser() + "\", \"action\": \"WRITE\", \"method\": \"delete\"}");;
-        // Pour lever une la même exception si l'objet n'existe pas
+        //Pour lever une la même exception si l'objet n'existe pas
         findById(id);
         productRepository.deleteById(id);
     }
 
     public Product update(Product product) {
-        LoggerFactory.getLogger("profile.logger").info("{\"timestamp\": \"" + java.time.Instant.now() + "\", \"user\": \"" +UserContext.getCurrentUser() + "\", \"action\": \"WRITE\", \"method\": \"update\"}");;
-        // Pour lever une la même exception si l'objet n'existe pas
+        //Pour lever une la même exception si l'objet n'existe pas
         findById(product.getId());
         return productRepository.save(product);
     }
 
     public Product findMostExpensiveProduct() {
-        LoggerFactory.getLogger("profile.logger").info("{\"timestamp\": \"" + java.time.Instant.now() + "\", \"user\": \"" +UserContext.getCurrentUser() + "\", \"action\": \"EXPENSIVE\", \"method\": \"findMostExpensiveProduct\"}");;
         Optional<Product> target = productRepository.findFirstByOrderByPriceDesc();
         if (target.isPresent()) {
             return target.get();
-        } else {
+        }else{
             throw new ResourceNotFoundException("There is no such product");
         }
     }
